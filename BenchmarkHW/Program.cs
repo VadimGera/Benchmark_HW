aa984b2f6e7e34657d7495ad00625f25475ccc59
VadimGera <vadimnvidia@gmail.com>
2023-06-23 19:42:38 +0300

    Update Program.cs

--- BenchmarkHW/Program.cs
+++ BenchmarkHW/Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using BenchmarkDotNet.Attributes;
+﻿using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
 
 public class SearchBenchmark
